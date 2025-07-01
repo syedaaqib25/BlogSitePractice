1455c05f23c72b161ecfb9173286ae1c25c5ac47
Syed Aaqib <syedaqib2742@gmail.com>
2025-07-01 13:09:05 +0530

    Auto-create tables on startup

--- run.py
+++ run.py
@@ -2,7 +2,9 @@ from app import create_app, db
 
 app = create_app()
 
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()  # Creates the database tables
-    app.run(debug=True)
+# Auto-create tables on startup (for SQLite or initial deployment)
+with app.app_context():
+    db.create_all()
+
+if __name__ == "__main__":
+    app.run()
