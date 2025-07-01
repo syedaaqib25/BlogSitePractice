d414a4be3d3d10ba6efbc975c8dda1a9ef862d0f
Syed Aaqib <syedaqib2742@gmail.com>
2025-07-01 14:29:43 +0530

    Added migrations

--- app/config.py
+++ app/config.py
@@ -1,5 +1,7 @@
+import os
+
 class Config:
-    SECRET_KEY = 'your_secret_key_here'  # Replace with a secure key
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///blog.db'  # Use PostgreSQL/MySQL in production
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'your_fallback_secret')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///blog.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = 'app/static/uploads'
